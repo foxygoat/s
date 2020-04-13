5497d524c842c038a51857c2d2c56067534ed5ce
Julia Ogris <julia.ogris@gmail.com>
2020-04-14 00:15:41 +1000

    Initialise httpe package
    Implement core functionality of httpe package: an http.Handler that can
    return an error.
    
    	type HandlerE interface {
    	       ServeHTTP(http.ResponseWriter, *http.Request) error
    	}
    
    It is intended to be used with ErrWriter which handles the error and
    turns it into an appropriate http response StatusCode and Body.
    
    	type ErrWriter interface {
    	       Write(http.ResponseWriter, error)
    	}
    
    `Chain` chains several HandlerEs sequentially and bails on the first
    error.
    
    `NewHandler` creates a traditional `http.Handler` from a `HandlerE` and
    `ErrWriter` and `NewHandlerFunc` creates an `http.HandlerFunc` from
    `HandlerFuncE` and `ErrWriterFunc`.
    
    The httpe package also provides HandlerFuncE and ErrWriterFunc types as
    adapters to the interfaces, similar to http.HandlerFunc for http.Handler
    interface.
    
    Introduce new `mock` package exposing mock.ResponseWriter which
    implements http.ResponseWriter. It captures the bytes and http status
    code written and can be preset to respond with an error on Write calls.

---
+++ httpe/example_httpe_test.go
@@ -0,0 +1,62 @@
+package httpe_test
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"strings"
+
+	"foxygo.at/s/httpe"
+)
+
+type User struct {
+	Name string
+	Age  int
+}
+
+var (
+	errInput     = errors.New("input error")
+	errDuplicate = errors.New("duplicate")
+	storage      = map[string]User{}
+)
+
+func handle(w http.ResponseWriter, r *http.Request) error {
+	user := User{}
+	body, _ := ioutil.ReadAll(r.Body)
+	if err := json.Unmarshal(body, &user); err != nil {
+		return errInput
+	}
+	if user.Name == "" || user.Age < 0 {
+		return errInput
+	}
+	if _, ok := storage[user.Name]; ok {
+		return errDuplicate
+	}
+	storage[user.Name] = user
+	return nil
+}
+
+func writeErr(w http.ResponseWriter, err error) {
+	switch {
+	case errors.Is(err, errInput):
+		http.Error(w, err.Error(), http.StatusBadRequest)
+	case errors.Is(err, errDuplicate):
+		http.Error(w, "duplicate user", http.StatusForbidden)
+	default:
+		http.Error(w, "something went wrong", http.StatusInternalServerError)
+	}
+}
+
+func ExampleHandlerFuncE() {
+	handler := httpe.NewHandlerFunc(handle, writeErr)
+	// http.ListenAndServe(":9090", handler)
+
+	w := httptest.NewRecorder()
+	r := httptest.NewRequest("POST", "/user", strings.NewReader(`{"Name": "truncated...`))
+	handler(w, r)
+	fmt.Printf("%d %s", w.Code, w.Body.String())
+	// output: 400 input error
+}
---
+++ httpe/httpe_test.go
@@ -0,0 +1,67 @@
+package httpe
+
+import (
+	"fmt"
+	"net/http"
+	"testing"
+
+	"foxygo.at/s/mock"
+	"github.com/stretchr/testify/require"
+)
+
+var errNoPost = fmt.Errorf("no POST method")
+
+type handlerE struct{}
+
+func (*handlerE) ServeHTTPe(w http.ResponseWriter, r *http.Request) error {
+	if r.Method != http.MethodPost {
+		return errNoPost
+	}
+	return nil
+}
+
+func (*handlerE) WriteErr(w http.ResponseWriter, err error) {
+	fmt.Fprint(w, err.Error())
+}
+
+func TestHandler(t *testing.T) {
+	he := &handlerE{}
+	h := NewHandler(he, he)
+	r := &http.Request{Method: http.MethodGet}
+	w := mock.ResponseWriter()
+	h.ServeHTTP(w, r)
+	require.Equal(t, errNoPost.Error(), w.LastBody)
+}
+
+func TestHandlerFunc(t *testing.T) {
+	he := handlerE{}
+	h := NewHandlerFunc(he.ServeHTTPe, he.WriteErr)
+	r := &http.Request{Method: http.MethodGet}
+	w := mock.ResponseWriter()
+	h.ServeHTTP(w, r)
+	require.Equal(t, errNoPost.Error(), w.LastBody)
+}
+
+func TestChain(t *testing.T) {
+	count := 0
+	errHand := fmt.Errorf("error 🤚")
+	goodHandler := HandlerFuncE(func(http.ResponseWriter, *http.Request) error {
+		count++
+		return nil
+	})
+	errorHandler := HandlerFuncE(func(http.ResponseWriter, *http.Request) error {
+		return errHand
+	})
+
+	h := Chain(goodHandler, goodHandler, goodHandler)
+	err := h.ServeHTTPe(mock.ResponseWriter(), &http.Request{})
+	require.NoError(t, err)
+	require.Equal(t, count, 3)
+
+	count = 0
+	h = Chain(goodHandler, errorHandler, goodHandler)
+	err = h.ServeHTTPe(mock.ResponseWriter(), &http.Request{})
+	require.Error(t, err)
+	require.Equal(t, err, errHand)
+	require.Equal(t, count, 1)
+}
