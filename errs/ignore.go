53a50420a0966b2da3f2720eb18c4c224305927e
Cam Hutchison <camh@xdna.net>
2020-10-08 13:08:59 +1100

    Revert "errs: Add Ignore function"
    The errs.Ignore function did not end up working the way we wanted it to,
    as lint could no longer tell that `Close` was being called on
    `Request.Body` since the `Close` function was passed by value to be
    executed anonymously.
    
    Since the main purpose was to have code that documented explicitly that
    we were ignoring errors and not have lint warnings, and that did not
    work, there is no purpose to this code any more.
    
    This reverts commit 1ed685a7ff1ca894d766b17ac65f38e0ecb1ca2e.

--- errs/ignore.go
+++
@@ -1,9 +0,0 @@
-package errs
-
-// Ignore executes nullary input function f and ignores its error return
-// value. A typical use case is ignoring errors in the defer statements:
-//
-//   defer errs.Ignore(body.Close)
-func Ignore(f func() error) {
-	_ = f()
-}
